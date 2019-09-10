ee584ad4b327f44c666fcb338126f3bd7eb79787
tehZevo <tehzevo@users.noreply.github.com>
2019-09-10 15:08:10 -0400

    Add scale exploration function support

--- pset/pset.py
+++ pset/pset.py
@@ -4,14 +4,16 @@ import tensorflow as tf
 def create_traces(model):
   return [np.zeros_like(w) for w in model.get_weights()]
 
-def get_action_and_gradient(model, state, tau, scale_exploration):
+def get_action_and_gradient(model, state, tau, scale_exploration=False):
   #get weights from model
   theta = model.get_weights()
   #modulate weights for exploration
-  if scale_exploration:
-    theta_pred = [np.random.normal(w, np.abs(w) * tau) for w in theta]
-  else:
+  if scale_exploration == True:
     theta_pred = [w.copy() + np.random.normal(size=w.shape) * tau for w in theta]
+  elif callable(scale_exploration):
+    theta_pred = [w.copy() + np.random.normal(size=w.shape) * tau * scale_exploration(w) for w in theta]
+  else:
+    theta_pred = [np.random.normal(w, np.abs(w) * tau) for w in theta]
   model.set_weights(theta_pred)
   #predict action
   action = model.predict(np.expand_dims(state, 0))[0]
